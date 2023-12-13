03f94136a28540141f71c69d59ecf9bf005e87d4
anaresgalla <anaresgalla@gmail.com>
2023-12-13 15:43:49 -0300

    API incompleta

--- spec/requests/apis/recipes_api_spec.rb
+++ spec/requests/apis/recipes_api_spec.rb
@@ -1,8 +1,16 @@
 require 'rails_helper'
   describe 'Recipes API' do
-    context 'lista todas as receitas' do 
-      user = create(:user, email: 'user@email.com', password: '123456', role: :user)
-      recipe = Recipe.create!(title: 'Brigadeiro', cook_time: '10')
+    context 'GET /api/v1/recipes' do 
+      it 'lista todas as receitas' do
+        user = create(:user, email: 'user@email.com', password: '123456', role: :user)
+        recipe = Recipe.create!(title: 'Brigadeiro', cook_time: '10')
 
+        get "api/v1/recipes"
+        expect(response.status).to eq 200
+        expect(response.content_type).to include 'application/json'
+        json_response = JSON.parse(response.body)
+        
+        expect(json-response.length).to eq 1
+      end 
   end 
 end 
